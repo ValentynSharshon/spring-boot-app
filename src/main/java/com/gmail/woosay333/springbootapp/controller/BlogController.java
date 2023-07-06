45e911e5a604d6f9f41d9bc72ca45e865c8cdc75
Valentyn Sharshon <woosay333@gmail.com>
2023-07-06 20:28:35 +0300

    add blogAdd and blogPostAdd methods

--- src/main/java/com/gmail/woosay333/springbootapp/controller/BlogController.java
+++ src/main/java/com/gmail/woosay333/springbootapp/controller/BlogController.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlogController {
 
     @Autowired
     private PostRepository postRepository;
-
 
     @GetMapping("/blog")
     public String blogMain(Model model) {
@@ -22,6 +23,20 @@ public class BlogController {
         model.addAttribute("posts", posts);
 
         return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        model.addAttribute("title", "Create post page");
+
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@ModelAttribute("post") Post post) {
+        postRepository.save(post);
+
+        return "redirect:/blog";
     }
 
 }
